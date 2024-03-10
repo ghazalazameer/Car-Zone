82ef0e7c0d54080750f039da12245896fbc3261a
ghazalazameer <zameerghazala20@gmail.com>
2024-03-10 21:51:28 +0530

    added cars (to be continued..)

--- carzone/settings.py
+++ carzone/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cars.apps.PagesConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
